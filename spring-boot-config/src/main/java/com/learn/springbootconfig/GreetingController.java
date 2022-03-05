9c162c7eb4635a9ae726fbddab2cb856f25ff4ff
Wissal Salhi <wissalagueb@gmail.com>
2022-03-05 19:07:39 +0100

    Spring Cloud config (server + clients)

--- spring-boot-config/src/main/java/com/learn/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/learn/springbootconfig/GreetingController.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 
 	@Value("${my.greeting: default value}")
@@ -30,7 +32,7 @@ public class GreetingController {
 	
 	@RequestMapping("/greeting")
 	public String greeting() {
-		return dbSettings.getValues() + " "+ dbSettings.getHost() + " "+ dbSettings.getPort();
+		return myGreeting + " " + dbSettings.getValues() + " "+ dbSettings.getHost() + " "+ dbSettings.getPort();
 		//return myGreeting + " "+ listValues + " "+ dbValues;
 	}
 	
